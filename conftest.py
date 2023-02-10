5f1c3d1f9f024bd5aea7a11e4cec5e93a73c0c99
Anna <you@example.com>
2023-02-10 14:27:54 +0300

    add ff+finalizer

--- conftest.py
+++ conftest.py
@@ -1,9 +1,30 @@
 import pytest
+import os
 from selenium import webdriver
 
-DRIVERS_STORAGE = "/Users/Anna/Desktop/Drivers/chromedriver.exe"
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", default="chrome", help="Browser to run tests"
+    )
+    parser.addoption(
+        "--driver_storage", default=os.path.expanduser("~/Users/Anna/Desktop/Drivers"),
+        help="Browser to run tests"
+    )
 
 
 @pytest.fixture()
-def driver():
-    return webdriver.Chrome(executable_path=f"{DRIVERS_STORAGE}/chromedriver")
+def driver(request):
+    browser_name = request.config.getoption("--browser")
+    driver_storage = request.config.getoption("--driver_storage")
+
+    _driver = None
+
+    if browser_name == "chrom":
+        _driver = webdriver.Chrome(executable_path=f"{driver_storage}/chromedriver")
+    elif browser_name == "firefox" or browser_name == "ff":
+        _driver = webdriver.Firefox(executable_path=f"{driver_storage}/geckodriver")
+
+    yield _driver
+
+    _driver.close()
